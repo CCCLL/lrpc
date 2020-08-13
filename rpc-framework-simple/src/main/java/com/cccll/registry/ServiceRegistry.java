f1ce30f94198abd9ffa1ddd284e4c0029a2a7ee4
CCCLL <cccll2019@gmail.com>
2020-08-13 16:30:51 +0800

    定义服务注册接口

---
+++ rpc-framework-simple/src/main/java/com/cccll/registry/ServiceRegistry.java
@@ -0,0 +1,14 @@
+package com.cccll.registry;
+
+import java.net.InetSocketAddress;
+
+public interface ServiceRegistry {
+    /**
+     * register service
+     *
+     * @param rpcServiceName    rpc service name
+     * @param inetSocketAddress service address
+     */
+    void registerService(String rpcServiceName, InetSocketAddress inetSocketAddress);
+
+}
