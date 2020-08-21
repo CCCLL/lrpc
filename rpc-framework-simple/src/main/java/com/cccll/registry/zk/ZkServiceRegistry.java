84606aecb185600b9a4bfd2eba0e42c6ef3d4286
CCCLL <cccll2019@gmail.com>
2020-08-21 18:19:36 +0800

    feat:服务注册类开发

---
+++ rpc-framework-simple/src/main/java/com/cccll/registry/zk/ZkServiceRegistry.java
@@ -0,0 +1,25 @@
+package com.cccll.registry.zk;
+
+import com.cccll.registry.ServiceRegistry;
+import com.cccll.registry.zk.util.CuratorUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+
+import java.net.InetSocketAddress;
+
+/**
+ * service registration  based on zookeeper
+ *
+ * @author cccll
+ * @createTime 2020年06月30日 10:56:00
+ */
+@Slf4j
+public class ZkServiceRegistry implements ServiceRegistry {
+    @Override
+    public void registerService(String rpcServiceName, InetSocketAddress inetSocketAddress) {
+        String servicePath = CuratorUtils.ZK_REGISTER_ROOT_PATH + "/" + rpcServiceName + inetSocketAddress.toString();
+        CuratorFramework zkClient = CuratorUtils.getZkClient();
+        CuratorUtils.createPersistentNode(zkClient, servicePath);
+
+    }
+}
