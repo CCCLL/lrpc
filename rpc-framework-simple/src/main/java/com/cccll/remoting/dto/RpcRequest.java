f6a91b31d910abf17c45fb4f8a31305c0f575ddb
CCCLL <cccll2019@gmail.com>
2020-09-04 18:34:57 +0800

    feat:request实体类开发

---
+++ rpc-framework-simple/src/main/java/com/cccll/remoting/dto/RpcRequest.java
@@ -0,0 +1,32 @@
+package com.cccll.remoting.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * @author cccll
+ * @createTime 2020年05月10日 08:24:00
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@ToString
+public class RpcRequest implements Serializable {
+    private static final long serialVersionUID = 1905122041950251207L;
+    private String requestId;
+    private String interfaceName;
+    private String methodName;
+    private Object[] parameters;
+    private Class<?>[] paramTypes;
+    private RpcMessageType rpcMessageType;
+    private String version;
+    private String group;
+
+    public RpcServiceProperties toRpcProperties() {
+        return RpcServiceProperties.builder().serviceName(this.getInterfaceName())
+                .version(this.getVersion())
+                .group(this.getGroup()).build();
+    }
+}
