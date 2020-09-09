7aa299dd81e9775f8e7adb5cc6288473b291ada1
CCCLL <cccll2019@gmail.com>
2020-09-09 21:28:28 +0800

    feat:服务提供相关管理接口和实现开发

---
+++ rpc-framework-simple/src/main/java/com/cccll/provider/ServiceProvider.java
@@ -0,0 +1,27 @@
+package com.cccll.provider;
+
+/**
+ * 保存和提供服务实例对象。服务端使用。
+ *
+ * @author cccll
+ * @createTime 2020年05月31日 16:52:00
+ */
+public interface ServiceProvider {
+    /**
+     * 保存服务实例对象和服务实例对象实现的接口类的对应关系
+     *
+     * @param service      服务实例对象
+     * @param serviceClass 服务实例对象实现的接口类
+     * @param <T>          服务接口的类型
+     */
+    <T> void addServiceProvider(T service, Class<T> serviceClass);
+
+    /**
+     * 获取服务实例对象
+     *
+     * @param serviceName 服务实例对象实现的接口类的类名
+     * @return 服务实例对象
+     */
+    Object getServiceProvider(String serviceName);
+
+}
---
+++ rpc-framework-simple/src/main/java/com/cccll/provider/ServiceProviderImpl.java
@@ -0,0 +1,50 @@
+package com.cccll.provider;
+
+import com.cccll.exception.RpcException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 实现了 ServiceProvider 接口，可以将其看做是一个保存和提供服务实例对象的示例
+ *
+ * @author cccll
+ * @createTime 2020年05月13日 11:23:00
+ */
+@Slf4j
+public class ServiceProviderImpl implements ServiceProvider{
+    /**
+     * 接口名和服务的对应关系
+     * note:处理一个接口被两个实现类实现的情况如何处理？（通过 group 分组）
+     * key:service/interface name
+     * value:service
+     */
+    private static Map<String, Object> serviceMap = new ConcurrentHashMap<>();
+    private static Set<String> registeredService = ConcurrentHashMap.newKeySet();
+
+    /**
+     * note:可以修改为扫描注解注册
+     */
+    @Override
+    public <T> void addServiceProvider(T service, Class<T> serviceClass) {
+        String serviceName = serviceClass.getCanonicalName();
+        if (registeredService.contains(serviceName)) {
+            return;
+        }
+        registeredService.add(serviceName);
+        serviceMap.put(serviceName, service);
+        log.info("Add service: {} and interfaces:{}", serviceName, service.getClass().getInterfaces());
+    }
+
+    @Override
+    public Object getServiceProvider(String serviceName) {
+        Object service = serviceMap.get(serviceName);
+        if (null == service) {
+            throw new RpcException(RpcErrorMessageEnum.SERVICE_CAN_NOT_BE_FOUND);
+        }
+        return service;
+    }
+
+}
