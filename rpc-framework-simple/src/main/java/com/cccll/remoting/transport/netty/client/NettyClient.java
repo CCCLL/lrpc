334d2b26163915fc4c8660224a0351b48c0e250b
CCCLL <cccll2019@gmail.com>
2020-10-08 23:24:31 +0800

    初步开发netty客户端

---
+++ rpc-framework-simple/src/main/java/com/cccll/remoting/transport/netty/client/NettyClient.java
@@ -0,0 +1,90 @@
+package com.cccll.remoting.transport.netty.client;
+
+import com.cccll.remoting.dto.RpcRequest;
+import com.cccll.remoting.dto.RpcResponse;
+import com.cccll.serialize.Serializer;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 初始化和关闭Bootstrap对象
+ *
+ * @author cccll
+ * @createTime 2020年07月01日 22:51:00
+ */
+@Slf4j
+public final class NettyClient {
+    private final Bootstrap bootstrap;
+    private final EventLoopGroup eventLoopGroup;
+
+    // 初始化资源，例如EventLoopGroup，Bootstrap
+    public NettyClient() {
+        eventLoopGroup = new NioEventLoopGroup();
+        bootstrap = new Bootstrap();
+        Serializer kryoSerializer = ExtensionLoader.getExtensionLoader(Serializer.class).getExtension("kyro");
+        bootstrap.group(eventLoopGroup)
+                .channel(NioSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                //  连接的超时时间.
+                //  如果超过该时间或无法建立连接，则连接失败。
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        // 如果在15秒内没有数据发送到服务器，则发送一个心跳请求
+                        ch.pipeline().addLast(new IdleStateHandler(0, 5, 0, TimeUnit.SECONDS));
+                        /*
+                         配置自定义序列化编解码器
+                         */
+                        // RpcResponse -> ByteBuf
+                        ch.pipeline().addLast(new NettyKryoDecoder(kryoSerializer, RpcResponse.class));
+                        // ByteBuf -> RpcRequest
+                        ch.pipeline().addLast(new NettyKryoEncoder(kryoSerializer, RpcRequest.class));
+                        ch.pipeline().addLast(new NettyClientHandler());
+                    }
+                });
+    }
+
+
+    /**
+     * 连接服务器并获取channel，这样你就可以向服务器发送rpc消息
+     *
+     * @param inetSocketAddress server address
+     * @return the channel
+     */
+    @SneakyThrows
+    public Channel doConnect(InetSocketAddress inetSocketAddress) {
+        CompletableFuture<Channel> completableFuture = new CompletableFuture<>();
+        bootstrap.connect(inetSocketAddress).addListener((ChannelFutureListener) future -> {
+            if (future.isSuccess()) {
+                log.info("The client has connected [{}] successful!", inetSocketAddress.toString());
+                completableFuture.complete(future.channel());
+            } else {
+                throw new IllegalStateException();
+            }
+        });
+        return completableFuture.get();
+    }
+
+    public void close() {
+        eventLoopGroup.shutdownGracefully();
+    }
+
+}
