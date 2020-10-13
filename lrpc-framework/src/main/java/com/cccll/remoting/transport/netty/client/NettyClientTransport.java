8b06aa31d2e329bf7246b31251f464561bda5637
CCCLL <cccll2019@gmail.com>
2020-10-13 23:18:58 +0800

    feat：基于netty传输RpcRequest

---
+++ lrpc-framework/src/main/java/com/cccll/remoting/transport/netty/client/NettyClientTransport.java
@@ -0,0 +1,56 @@
+package com.cccll.remoting.transport.netty.client;
+
+import com.cccll.factory.SingletonFactory;
+import com.cccll.registry.ServiceDiscovery;
+import com.cccll.registry.zk.ZkServiceDiscovery;
+import com.cccll.remoting.dto.RpcRequest;
+import com.cccll.remoting.dto.RpcResponse;
+import com.cccll.remoting.transport.ClientTransport;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * 基于 Netty 传输 RpcRequest。
+ *
+ * @author cccll
+ */
+@Slf4j
+public class NettyClientTransport implements ClientTransport {
+    private final ServiceDiscovery serviceDiscovery;
+    private final UnprocessedRequests unprocessedRequests;
+
+    public NettyClientTransport() {
+        this.serviceDiscovery = new ZkServiceDiscovery();
+        this.unprocessedRequests = SingletonFactory.getInstance(UnprocessedRequests.class);
+    }
+
+    @Override
+    public CompletableFuture<RpcResponse> sendRpcRequest(RpcRequest rpcRequest) {
+        // 构建返回值
+        CompletableFuture<RpcResponse> resultFuture = new CompletableFuture<>();
+        InetSocketAddress inetSocketAddress = serviceDiscovery.lookupService(rpcRequest.getInterfaceName());
+        Channel channel = ChannelProvider.get(inetSocketAddress);
+        if (channel != null && channel.isActive()) {
+            // 放入未处理的请求
+            unprocessedRequests.put(rpcRequest.getRequestId(), resultFuture);
+            channel.writeAndFlush(rpcRequest).addListener((ChannelFutureListener) future -> {
+                if (future.isSuccess()) {
+                    log.info("client send message: [{}]", rpcRequest);
+                } else {
+                    future.channel().close();
+                    resultFuture.completeExceptionally(future.cause());
+                    log.error("Send failed:", future.cause());
+                }
+            });
+        } else {
+            throw new IllegalStateException();
+        }
+
+        return resultFuture;
+    }
+
+}
